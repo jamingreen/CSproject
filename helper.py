ce68a4f31936b72bb79521f5195741c1e21af884
Jamin252 <jamiegr252@gmail.com>
2022-06-22 23:10:44 +0100

    Finish level 1 draw

--- helper.py
+++ helper.py
@@ -1,98 +1,63 @@
 import csv, pygame
 from os import read
-from mario import Game
-
-#Global Variables / Constants
-BROWN = (135, 52, 35)
-SKYBLUE = (91, 148, 251)
-LIGHTGREY = (213, 216, 220)
-SIZE = (800,500)
-BLOCKSIZE = (50,50)
-
-class Map():
-    
-    def __init__(self, filename):
-        temp = []
-        with open(filename, 'r') as f:
-            reader =  csv.reader(f)
-            next(reader)
-            
-            for row in reader:
-                temp.append(tuple(row))
-        self.groundSpriteGroup = pygame.sprite.Group()
-        self.tileGroup = pygame.sprite.Group()
-        self.parseMap(temp)
-        
-
-    def parseMap(self,tiles):
-        for row in tiles:
-            if row[0] == "ground":
-                groundTile = Ground((row[1], row[2]))
-                self.groundSpriteGroup.add(groundTile)
-                self.tileGroup.add(groundTile)
-            elif row[0] == "airTile":
-                airTile = airTile((row[1], row[2]))
-                self.tileGroup.add(airTile)
-
-    def draw(self,screen):
-        self.tileGroup.draw(screen)
-
-
-class Tile(pygame.sprite.Sprite):
-
-    def __init__(self,position):
-        super().__init__()
-        self.x = position[0]
-        self.y = position[1]
-    
-
-class Ground(Tile):
-
-    def __init__(self,position):
-        super().__init__(position)
-        self.image = pygame.transform.scale(pygame.image.load("images/groundTile.png"),BLOCKSIZE)
-        self.rect = self.image.get_rect()
-        self.rect.x = self.x
-        self.rect.y = self.y
-
-class AirTile(Tile):
-    
-    def __init__(self,position):
-        super().__init__(position)
-        self.image = pygame.transform.scale(pygame.image.load("images/airTile.png"),BLOCKSIZE)
-        self.rect = self.image.get_rect()
-        self.rect.x = self.x
-        self.rect.y = self.y
+from constants import *
 
 class Button(pygame.sprite.Sprite):
 
     def __init__(self,width, height, position, imageName):
+        pygame.sprite.Sprite.__init__(self)
         self.image = pygame.transform.scale(pygame.image.load(imageName), (width, height))
         self.rect = self.image.get_rect()
         self.rect.x = position[0]
         self.rect.y = position[1]
 
-    def mouseInteraction(self,position):
-        if position[0] >= self.position[0] and position[0] <= self.position[0] + self.width and position[1] >= self.position[1] and position[1] <= self.position[1] + self.height:
-            return True
-        else:
-            return False
+    def mouseInteraction(self,position, status):
+        if self.rect.collidepoint(position):
+            pass
+        return status
+
+    def draw(self,screen):
+        screen.blit(self.image, (self.rect.x,self.rect.y))
+
 
 class SettingButton(Button):
 
     def __init__(self, width, height, position):
-        super().__init__(width, height,position,"image/settingButton.png")
+        super().__init__(width, height,position,"images/settingButton.png")
+
+    def mouseInteraction(self,position, status):
+        if self.rect.collidepoint(position):
+            print("Setting Button mouse collide")
+            status.extend(SCREENTOSETTING)
+        return status
     
-class LevelButton(Button):
 
-    def __init__(self,name, width, height, position, imageName):
-        super().__init__(width, height, position, imageName)
-        self.name = name
+def relativeCoor2DeCoor(relativePosition):
+    return (relativePosition[0]*BLOCKSIZE[0], relativePosition[1]*BLOCKSIZE[1])
 
-    def mouseInteraction(self,position):
-        if super().mouseInteraction(position):
-            self.initialiseGame()
+def deCoor2RelativeCoor(dePosition):
+    return (dePosition[0]/BLOCKSIZE[0], dePosition[1]/BLOCKSIZE[1])
 
-    def initialiseGame(self):
-        pass
 
+class MouseBuffer():
+
+    def __init__(self):
+        self.timer = 0
+        self.flag = False
+        self.frameNum = 8
+
+    def tFlag(self):
+        self.flag = True
+
+    def logic(self):
+        if self.flag:
+            self.count()
+        if self.timer >= self.frameNum:
+            self.flag = False
+            self.reset()
+    
+    def reset(self):
+        self.timer = 0
+    
+    def count(self):
+        self.timer +=1
--- main1.py
+++ main1.py
@@ -3,17 +3,7 @@ from jinja2 import pass_context
 import pygame, math, sys
 from helper import *
 from mario import Game
-
-#Global constant
-SIZE = (800,500)
-SKYBLUE = (91, 148, 251)
-EXIT = "exit"
-SETTINGS = (80,80)
-LEVELBUTTONSIZE = (148, 40)
-LEVELIMAGESIZE = (240, 168)
-LEVELIMAGEBUTTONPADDING = EDGELEVELIMAGEPADDING = 16
-LEVEL2LEVELPADDING = 24
-NUMOFLEVELS = 3
+from constants import *
 
 
 class Main():
@@ -23,34 +13,53 @@ class Main():
         self.screen = pygame.display.set_mode(self.size)
         self.surface = pygame.display.set_caption("Mario")
         self.clock = pygame.time.Clock()
-        self.all_sprites_group = pygame.sprite.Group()
+        self.all_sprites_list = []
+        self.mouseBuffer = MouseBuffer()
+        self.currentScreen = MENU
+
+        # Objects
         self.menu = Menu(self.screen)
         self.setting = Setting()
-        self.settingButton = SettingButton()
-        self.levelButtonGroup = pygame.sprite.Group()
-        self.menuSpriteGroup = pygame.sprite.Group()
-        self.status = ["start"]
         
-        startY = self.size[1] / 2 + LEVELIMAGESIZE[1]/2 + LEVELIMAGEBUTTONPADDING
-        startX = self.size[0] / 2 - LEVELIMAGESIZE[0] * 1.5 - LEVEL2LEVELPADDING + (LEVELIMAGESIZE[0] - LEVELBUTTONSIZE[0])/2
+        self.all_sprites_list.extend([self.menu, self.setting])
 
-        for lvlNum in range(NUMOFLEVELS):
-            levelBut = LevelButton(f"Level {lvlNum + 1}",*LEVELBUTTONSIZE,(startX,startY), "tempLevelImg.png")
-            self.levelButtonGroup.add(levelBut)
-            self.menuSpriteGroup.add(levelBut)
-            self.all_sprites_group.add(levelBut)
+        # Sprite Groups
+        self.status = []
+        
 
     def keyResponse(self,event):
         pass
 
+    # Change status (Task pending to do)
     def mouseResponse(self, position):
-        pass
+        if self.currentScreen == MENU:
+            self.status = self.menu.mouseInteraction(position, self.status)
+        elif self.currentScreen == SETTINGSCREEN:
+            self.status = self.setting.mouseInteraction(position, self.status)
     
+    def setCurrentScreen(self, newScreen):
+        self.currentScreen = newScreen
+
+    def readStatus(self):
+        i = 0
+        while i < len(self.status):
+            stat = self.status[i]
+            if stat == SCREENTOSETTING:
+                self.setCurrentScreen(SETTINGSCREEN)
+            del self.status[i]
+
     def logic(self):
+        self.mouseBuffer.logic()
         pass
 
     def drawScreen(self):
-        pass
+        self.screen.fill(BROWN)
+        if self.currentScreen == MENU:
+            self.menu.drawScreen(self.screen)
+        elif self.currentScreen == SETTINGSCREEN:
+            self.setting.drawScreen(self.screen)
+        
+        pygame.display.flip()
 
     def gameplay(self):
         done = False
@@ -62,9 +71,11 @@ class Main():
                 if event.type == pygame.QUIT:
                     sys.exit()
                 self.keyResponse(event)
-            
+                    
             click,_,_ = pygame.mouse.get_pressed()
-            if click == True:
+            if click == True and not self.mouseBuffer.flag:
+                self.mouseBuffer.tFlag()
+                print("Left click")
                 mouse = pygame.mouse.get_pos()
                 self.mouseResponse(mouse)
 
@@ -87,7 +98,51 @@ class Main():
 class Menu():
 
     def __init__(self, screen):
-        pass
+        self.size = SIZE
+        self.screen = screen
+        self.menuSpriteGroup = pygame.sprite.Group()
+        self.levelButtonGroup = pygame.sprite.Group()
+
+        self.settingButton = SettingButton(*SETTINGS_BUTTON_SIZE, (SIZE[0] - 132, 20))
+        self.menuSpriteGroup.add(self.settingButton)
+        
+        startY = 120 + LEVEL_IMAGE_SIZE[1] + LEVEL_IMAGE_BUTTON_PADDING
+        startX = EDGE_LEVEL_IMAGE_PADDING + (LEVEL_IMAGE_SIZE[0]-LEVEL_BUTTON_SIZE[0])/2
+
+
+        for lvlNum in range(NUM_OF_LEVELS):
+            levelBut = LevelButton(lvlNum+1, f"Level {lvlNum + 1}",*LEVEL_BUTTON_SIZE,(startX,startY), "images/tempLevelImg.png")
+            self.levelButtonGroup.add(levelBut)
+            self.menuSpriteGroup.add(levelBut)
+            startX += 264
+
+    # Return new status
+    def mouseInteraction(self, position, status):
+        for sprite in self.menuSpriteGroup:
+            status = sprite.mouseInteraction(position, status)
+        return status
+
+    def drawScreen(self,screen):
+        self.settingButton.draw(screen)
+        for sprite in self.menuSpriteGroup:
+            sprite.draw(screen)
+
+class LevelButton(Button):
+
+    def __init__(self,level, name, width, height, position, imageName):
+        super().__init__(width, height, position, imageName)
+        self.name = name
+        self.level = level
+
+    def mouseInteraction(self,position, status):
+        if self.rect.collidepoint(position):
+            print("Level button mouse collide")
+            self.initialiseGame()
+        return status
+
+    def initialiseGame(self):
+        game = Game(self.level)
+        game.play()
 
 
 # Setting Screen
@@ -95,11 +150,24 @@ class Setting():
 
     def __init__(self):
         pass
+    
+
+    def mouseInteraction(self,position, status):
+        pass
+
+    def drawScreen(self, screen):
+        pass
 
 # Ranking Screen
 class RankingScreen():
 
     def __init__(self):
+        pass
+
+    def mouseInteraction(self,position, status):
+        pass
+
+    def drawScreen(self, screen):
         pass
 
 if __name__ == "__main__":
